09094e52a110814ac80628e2b7668d0a8980a704
Dimas Alamsyah <dimasalamsyah0712@gmail.com>
2020-12-07 22:06:47 +0700

    add new header

--- application/views/header.php
+++ application/views/header.php
@@ -29,19 +29,60 @@
     
   </style>
 
-  	<!-- As a link -->
-	<nav class="navbar navbar-light bg-light">
+  <!-- As a link -->
+	<!-- <nav class="navbar navbar-light bg-light">
     <a class="navbar-brand" href="#">Sistem Informasi Pendaftaran</a>
+    <div class="btn-group">
+      <label type="button" class="dropdown-toggle" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+        <?php echo $this->session->nama; ?>
+      </label>
+      <div class="dropdown-menu dropdown-menu-right">
+        <a href="<?php echo base_url(). 'logout'; ?>" class="dropdown-item" type="button">Keluar</a>
+      </div>
+    </div>
+	</nav> -->
+
+  <nav class="navbar navbar-expand-lg navbar-light bg-light">
+    <a class="navbar-brand" href="<?php echo base_url(); ?>">Sistem Informasi Pendaftaran</a>
+    <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNavDropdown" aria-controls="navbarNavDropdown" aria-expanded="false" aria-label="Toggle navigation">
+      <span class="navbar-toggler-icon"></span>
+    </button>
+    <div class="collapse navbar-collapse" id="navbarNavDropdown">
+      <ul class="navbar-nav">
+        <!-- <li class="nav-item active">
+          <a class="nav-link" href="#">Home <span class="sr-only">(current)</span></a>
+        </li> -->
+        <li class="nav-item dropdown">
+          <a class="nav-link dropdown-toggle" href="#" id="navbarDropdownMenuLink" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+            Master Data
+          </a>
+          <div class="dropdown-menu" aria-labelledby="navbarDropdownMenuLink">
+            <a class="dropdown-item" href="<?php echo base_url(). 'kelas'; ?>">Kelas</a>
+            <a class="dropdown-item" href="<?php echo base_url(). 'jurusan'; ?>">Jurusan</a>
+            <a class="dropdown-item" href="<?php echo base_url(). 'pelajaran'; ?>">Mata Pelajaran</a>
+            <a class="dropdown-item" href="<?php echo base_url(). 'siswa'; ?>">Siswa</a>
+          </div>
+        </li>
+        <li class="nav-item dropdown">
+          <a class="nav-link dropdown-toggle" href="#" id="navbarDropdownMenuLink" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+            Laporan
+          </a>
+          <div class="dropdown-menu" aria-labelledby="navbarDropdownMenuLink">
+            <a class="dropdown-item" href="#">Kelas</a>
+            <a class="dropdown-item" href="#">Jurusan</a>
+            <a class="dropdown-item" href="#">Mata Pelajaran</a>
+            <a class="dropdown-item" href="#">Siswa</a>
+          </div>
+        </li>
+      </ul>
+    </div>
 
     <div class="btn-group">
       <label type="button" class="dropdown-toggle" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
         <?php echo $this->session->nama; ?>
       </label>
-      <!-- <i style="padding: 10px;" class="fas fa-user-circle dropdown-toggle" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false"></i> -->
       <div class="dropdown-menu dropdown-menu-right">
-        <!-- <a class="dropdown-item" type="button">Ubah Password</a> -->
         <a href="<?php echo base_url(). 'logout'; ?>" class="dropdown-item" type="button">Keluar</a>
       </div>
     </div>
-
-	</nav>
+  </nav>
